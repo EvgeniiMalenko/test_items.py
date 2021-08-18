bcff953902eb8ee9ec69ff933efff849fb9d0324
EvgeniiMalenko <84942828+EvgeniiMalenko@users.noreply.github.com>
2021-08-18 10:46:46 -0700

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, 'Кнопка отсутствует'
